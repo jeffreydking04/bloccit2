43ee93776746485160e8f28d53214b82460cd809
Jeffrey D King <jdking33@aol.com>
2016-05-21 16:10:37 +0000

    Submit Assignment 31

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,12 @@
 class PostsController < ApplicationController
   def index
     @posts = Post.all
+    @posts.each do |post|
+      if post.id % 5 == 1
+        post.title = ("SPAM")
+        post.save!
+      end
+    end
   end
 
   def show
