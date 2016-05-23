052264718628ab7a6a1a200a0aef81a52c8e3c75
Jeffrey D King <jdking33@aol.com>
2016-05-23 19:06:54 +0000

    Submit Checkpoint 34

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,13 +1,12 @@
 class PostsController < ApplicationController
-  def index
-    @posts = Post.all
-  end
-
   def show
     @post = Post.find(params[:id])
+    @topic = Topic.find(params[:topic_id])
+    @post.topic = @topic    
   end
 
   def new
+    @topic = Topic.find(params[:topic_id])
     @post = Post.new
   end
   
@@ -15,10 +14,12 @@ class PostsController < ApplicationController
     @post = Post.new
     @post.title = params[:post][:title]
     @post.body = params[:post][:body]
+    @topic = Topic.find(params[:topic_id])
+    @post.topic = @topic
     
     if @post.save
       flash[:notice] = "Post was saved successfully."
-      redirect_to @post
+      redirect_to [@topic, @post]
     else
       flash.now[:alert] = "There was an error saving the post. Please try again."
       render :new
@@ -33,10 +34,12 @@ class PostsController < ApplicationController
     @post = Post.find(params[:id])
     @post.title = params[:post][:title]
     @post.body = params[:post][:body]
+    @topic = Topic.find(params[:topic_id]) 
+    @post.topic = @topic
     
     if @post.save
       flash[:notice] = "Post was updated successfully."
-      redirect_to @post
+      redirect_to([@topic, @post])
     else
       flash.now[alert] = "There was an error saving the post. Please try again."
       render :edit
@@ -45,10 +48,12 @@ class PostsController < ApplicationController
   
   def destroy
     @post = Post.find(params[:id])
+    @topic = Topic.find(params[:topic_id]) 
+    @post.topic = @topic    
     
     if @post.destroy
       flash[:notice] = "\"#{@post.title}\" was deleted successfully."
-      redirect_to posts_path
+      redirect_to @post.topic
     else
       flash.now[:alert] = "There was an error deleting the post."
       render :show
