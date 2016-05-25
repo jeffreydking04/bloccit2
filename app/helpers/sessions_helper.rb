a9de32222cbe8824199d952dd46c078d2b6e1195
Jeffrey D King <jdking33@aol.com>
2016-05-25 19:45:45 +0000

    Submit Assignment 38-Avatar displaying

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -13,6 +13,6 @@ module SessionsHelper
 
   def avatar_url(user)
     gravatar_id = Digest::MD5::hexdigest(user.email).downcase
-    "http://gravatar.com/avator/#{gravatar_id}.png?s=48"
+    "http://gravatar.com/avatar/#{gravatar_id}.png?s=48"
   end
 end
