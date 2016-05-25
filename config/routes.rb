5c2be7e145b264ece4aeaad38d0b808a6b335cae
Jeffrey D King <jdking33@aol.com>
2016-05-25 14:11:44 +0000

    Submit Assignment 37

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,8 @@ Rails.application.routes.draw do
 
   root 'welcome#index'
 
+  post 'users/confirm' => 'users#confirm'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
