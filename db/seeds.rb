d12587320eddb4e9b3777254de12a6775e9049a5
Jeffrey D King <jdking33@aol.com>
2016-05-26 21:51:18 +0000

    Restoring seed.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,55 +1,55 @@
-#require 'random_data'
-#
-#5.times do
-#    User.create!(
-#      name:     RandomData.random_name,
-#      email:    RandomData.random_email,
-#      password: RandomData.random_sentence
-#    )
-#end
-#users = User.all
-#
-#15.times do
-#  Topic.create!(
-#    name:        RandomData.random_sentence,
-#    description: RandomData.random_paragraph
-#  )
-#end
-#topics = Topic.all
-#
-#50.times do
-#  Post.create!(
-#    user:   users.sample,
-#    topic:   topics.sample,
-#    title:  RandomData.random_sentence,
-#    body:   RandomData.random_paragraph
-#  )
-#end
-#posts = Post.all
-#
-#100.times do
-#  Comment.create!(
-#    user: users.sample,
-#    post: posts.sample,
-#    body: RandomData.random_paragraph
-#  )
-#end
-#
-#admin = User.create!(
-#  name:     "Admin User",
-#  email:    "admin@example.com",
-#  password: "helloworld",
-#  role:     "admin"
-#)
-#
-#member = User.create!(
-#  name:     "Member User",
-#  email:    "member@example.com",
-#  password: "helloworld"
-#)
-#
-#puts "Seed finished"
-#puts "#{User.count} users created"
-#puts "#{Topic.count} topics created"
-#puts "#{Post.count} posts created"
-#puts "#{Comment.count} comments created"#
+require 'random_data'
+
+5.times do
+    User.create!(
+      name:     RandomData.random_name,
+      email:    RandomData.random_email,
+      password: RandomData.random_sentence
+    )
+end
+users = User.all
+
+15.times do
+  Topic.create!(
+    name:        RandomData.random_sentence,
+    description: RandomData.random_paragraph
+  )
+end
+topics = Topic.all
+
+50.times do
+  Post.create!(
+    user:   users.sample,
+    topic:   topics.sample,
+    title:  RandomData.random_sentence,
+    body:   RandomData.random_paragraph
+  )
+end
+posts = Post.all
+
+100.times do
+  Comment.create!(
+    user: users.sample,
+    post: posts.sample,
+    body: RandomData.random_paragraph
+  )
+end
+
+admin = User.create!(
+  name:     "Admin User",
+  email:    "admin@example.com",
+  password: "helloworld",
+  role:     "admin"
+)
+
+member = User.create!(
+  name:     "Member User",
+  email:    "member@example.com",
+  password: "helloworld"
+)
+
+puts "Seed finished"
+puts "#{User.count} users created"
+puts "#{Topic.count} topics created"
+puts "#{Post.count} posts created"
+puts "#{Comment.count} comments created"
