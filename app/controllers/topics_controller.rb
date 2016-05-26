bda8661a2c9f2533d891ee81f65e54db0f869987
Jeffrey D King <jdking33@aol.com>
2016-05-26 15:16:28 +0000

    Submit Checkpoint 40

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -1,19 +1,22 @@
 class TopicsController < ApplicationController
+  before_action :require_sign_in, except: [:index, :show]
+  before_action :authorize_user, except: [:index, :show]
+
   def index
     @topics = Topic.all
   end
-  
+
   def show
     @topic = Topic.find(params[:id])
   end
-  
+
   def new
     @topic = Topic.new
   end
-  
+
   def create
     @topic = Topic.new(topic_params)
-    
+
     if @topic.save
       flash[:notice] = "Topic was saved successfully."
       redirect_to(@topic)
@@ -22,15 +25,15 @@ class TopicsController < ApplicationController
       render :new
     end
   end
-  
+
   def edit
     @topic = Topic.find(params[:id])
   end
-  
+
   def update
     @topic = Topic.find(params[:id])
     @topic.assign_attributes(topic_params)
-    
+
     if @topic.save
       flash[:notice] = "Topic was updated successfully."
       redirect_to @topic
@@ -39,10 +42,10 @@ class TopicsController < ApplicationController
       render :edit
     end
   end
-  
+
   def destroy
     @topic = Topic.find(params[:id])
-    
+
     if @topic.destroy
       flash[:notice] = "\"#{@topic.name}\" was deleted successfully."
       redirect_to action: :index
@@ -56,5 +59,12 @@ class TopicsController < ApplicationController
 
   def topic_params
     params.require(:topic).permit(:name, :description, :public)
+  end
+
+  def authorize_user
+    unless current_user.admin?
+      flash[:alert] = "You must be an admin to do that."
+      redirect_to topics_path
+    end
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -14,26 +14,68 @@ RSpec.describe User, type: :model do
   it { is_expected.to validate_uniqueness_of(:email) }
   it { is_expected.to validate_length_of(:email).is_at_least(3) }
   it { is_expected.to allow_value("user@bloccit.com").for(:email) }
-  
+
   # Shoulda tests for password
   it { is_expected.to validate_presence_of(:password) }
   it { is_expected.to have_secure_password }
   it { is_expected.to validate_length_of(:password).is_at_least(6) }
-  
+
   describe "attributes" do
     it "should have name and email attributes" do
       expect(user).to have_attributes(name: "Bloccit User", email: "user@bloccit.com")
     end
+
+    it "should respond to role" do
+      expect(user).to respond_to(:role)
+    end
+
+    it "should respond to admin?" do
+      expect(user).to respond_to(:admin?)
+    end
+
+    it "should respond to member?" do
+      expect(user).to respond_to(:member?)
+    end
   end
-  
+
+  describe "roles" do
+    it "should be member by default" do
+      expect(user.role).to eq("member")
+    end
+
+    context "member user" do
+      it "should return true for #member?" do
+        expect(user.member?).to be_truthy
+      end
+
+      it "should return false for #admin?" do
+        expect(user.admin?).to be_falsey
+      end
+    end
+
+    context "admin user" do
+      before do
+        user.admin!
+      end
+
+      it "should return flase for #member?" do
+        expect(user.member?).to be_falsey
+      end
+
+      it "should return true for #admin?" do
+        expect(user.admin?).to be_truthy
+      end
+    end
+  end
+
   describe "invalid user" do 
     let(:user_with_invalid_name) { User.new(name: "", email: "user@bloccit.com") }
     let(:user_with_invalid_email) { User.new(name: "Bloccit User", email: "") }
-    
+
     it "should be an invalid user due to blank name" do
       expect(user_with_invalid_name).to_not be_valid
     end
-    
+
     it "should be an invalid user due to blank email" do
       expect(user_with_invalid_email).to_not be_valid
     end
