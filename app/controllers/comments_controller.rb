80b3e161612f533a312672abba4b47898a03d57f
Jeffrey D King <jdking33@aol.com>
2016-05-27 14:39:05 +0000

    Submit Assignment 42

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,20 +3,31 @@ class CommentsController < ApplicationController
   before_action :authorize_user, only: [:destroy]
 
   def create
-    @post = Post.find(params[:post_id])
-    comment = @post.comments.new(comment_params)
-    comment.user = current_user
+    if params[:post_id]
+      @post = Post.find(params[:post_id])
+      comment = @post.comments.new(comment_params)
+      comment.user = current_user
+    else
+      @topic = Topic.find(params[:topic_id])
+      comment = @topic.comments.new(comment_params)
+      comment.user = current_user
+    end
 
     comment.save ? flash[:notice] = "Comment saved successfully." : flash[:alert] = "Comment failed to save. Please try again."
-    redirect_to([@post.topic, @post])
+    @post ? redirect_to([@post.topic, @post]) : redirect_to(@topic)
   end
 
   def destroy
-    @post = Post.find(params[:post_id])
-    comment = @post.comments.find(params[:id])
+    if params[:post_id]
+      @post = Post.find(params[:post_id])
+      comment = @post.comments.find(params[:id])
+    else
+      @topic = Topic.find(params[:topic_id])
+      comment = @topic.comments.find(params[:id])
+    end
 
     comment.destroy ? flash[:notice] = "Comment was deleted successfully." : flash[:alert] = "Error deleting comment. Please try again."
-    redirect_to([@post.topic, @post])
+    @post ? redirect_to([@post.topic, @post]) : redirect_to(@topic)
   end
 
   private
