d630e66bccb4a79828ef9f5c8e3c01c44899427a
Jeffrey D King <jdking33@aol.com>
2016-05-27 17:25:10 +0000

    Submit Checkpoint 43

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -12,6 +12,7 @@ RSpec.describe Post, type: :model do
   it { is_expected.to have_many(:labelings) }
   it { is_expected.to have_many(:labels).through(:labelings) }
   it { is_expected.to have_many(:comments) }
+  it { is_expected.to have_many(:votes) }
   it { is_expected.to belong_to(:topic) }
   it { is_expected.to belong_to(:user) }
   it { is_expected.to validate_presence_of(:title) }
@@ -20,10 +21,56 @@ RSpec.describe Post, type: :model do
   it { is_expected.to validate_presence_of(:user) }
   it { is_expected.to validate_length_of(:title).is_at_least(5) }
   it { is_expected.to validate_length_of(:body).is_at_least(20) }
-  
+
   describe "attributes" do
     it "should have title, body, and user attributes" do
       expect(post).to have_attributes(title: title, body: body, user: user)
+    end
+  end
+
+  describe "voting" do
+    before do
+      3.times { post.votes.create!(value: 1) }
+      2.times { post.votes.create!(value: -1) }
+      @up_votes = post.votes.where(value: 1).count
+      @down_votes = post.votes.where(value: -1).count
+    end
+
+    describe "#up_votes" do
+      it "should count the number of votes with value = 1" do
+        expect( post.up_votes ).to eq(@up_votes)
+      end
+    end
+
+    describe "#down_votes" do
+      it "should count the number of votes with value = -1" do
+        expect( post.down_votes).to eq(@down_votes)
+      end
+    end
+
+    describe "#points" do
+      it "should return the sum of all down and up votes" do
+        expect( post.points ).to eq(@up_votes - @down_votes)
+      end
+    end
+
+    describe "#update_rank" do
+      it "should calculate the correct rank" do
+        post.update_rank
+        expect(post.rank).to eq(post.points + (post.created_at - Time.new(1970,1,1)) / 1.day.seconds)
+      end
+
+      it "should update the rank when an up vote is created" do
+        old_rank = post.rank
+        post.votes.create!(value: 1)
+        expect(post.rank).to eq(old_rank + 1)
+      end
+
+      it "should update the rank when a down vote is created" do
+        old_rank = post.rank
+        post.votes.create!(value: -1)
+        expect(post.rank).to eq(old_rank - 1)
+      end
     end
   end
 end
