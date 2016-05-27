b82aacc0df863bde0a846a71ffe5f60b8c30c477
Jeffrey D King <jdking33@aol.com>
2016-05-27 20:57:36 +0000

    Mailer issues unresolved

--- app/mailers/favorite_mailer.rb
+++ app/mailers/favorite_mailer.rb
@@ -2,9 +2,9 @@ class FavoriteMailer < ApplicationMailer
   default from: "jdking33@aol.com"
 
   def new_comment(user, post, comment)
-    #headers["Message-ID"] = "<comments/#{comment.id}@your-app-name.example>"
-    #headers["In-Reply-To"] = "<post/#{post.id}@your-app-name.example>"
-    #headers["References"] = "<post/#{post.id}@your-app-name.example>"
+    headers["Message-ID"] = "<comments/#{comment.id}@your-app-name.example>"
+    headers["In-Reply-To"] = "<post/#{post.id}@your-app-name.example>"
+    headers["References"] = "<post/#{post.id}@your-app-name.example>"
 
     @user = user
     @post = post
