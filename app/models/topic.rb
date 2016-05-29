b7aec0465dbcfbdc1e28ba83b3a775ba3f680137
Jeffrey D King <jdking33@aol.com>
2016-05-29 15:11:10 +0000

    Submit Assignment 46

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -6,5 +6,15 @@ class Topic < ActiveRecord::Base
   validates :name, length: { minimum: 5 }, presence: true
   validates :description, length: { minimum: 15 }, presence: true
 
-  scope :visible_to, -> (user) { user ? all : where(public: true) }
+  scope :visible_to, -> (user) { user ? all : self.publicly_viewable }
+
+  def self.publicly_viewable
+    publicly_viewable_topics = Topic.where(public: true)
+    publicly_viewable_topics
+  end
+
+  def self.privately_viewable
+    privately_viewable_topics = Topic.where(public: false)
+    privately_viewable_topics
+  end
 end
--- spec/models/topic_spec.rb
+++ spec/models/topic_spec.rb
@@ -37,5 +37,17 @@ RSpec.describe Topic, type: :model do
         expect(Topic.visible_to(nil)).to eq([@public_topic])
       end
     end
-   end
+
+    describe "pubicly_viewable" do
+      it "returns a collection of publicly viewable topis" do
+        expect(Topic.publicly_viewable).to eq(Topic.where(public: true))
+      end
+    end
+
+    describe "privately_viewable" do
+      it "returns a collection of privately viewable topis" do
+        expect(Topic.privately_viewable).to eq(Topic.where(public: false))
+      end
+    end
+  end
 end
