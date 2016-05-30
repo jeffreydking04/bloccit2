47f97fe7fa2b9e84c9f2e7355ac6448f0a44ff43
Jeffrey D King <jdking33@aol.com>
2016-05-30 18:56:03 +0000

    Submit Assignment 49 may need work

--- config/routes.rb
+++ config/routes.rb
@@ -23,7 +23,10 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :users, only: [:index, :show, :create, :update]
-      resources :topics, except: [:edit, :new]
+      resources :topics, except: [:edit, :new] do
+        resources :posts, only: [:create]
+      end
+      resources :posts, except: [:edit, :new]
     end
   end
 
