11ece6a8d9bd43fbfd5b9f2d43e4baa2d16ee272
Jeffrey D King <jdking33@aol.com>
2016-05-30 19:46:53 +0000

    Submit Assignment 49 routes.rb revision that allows endpoints to work as asked for in the assignment

--- config/routes.rb
+++ config/routes.rb
@@ -24,9 +24,8 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :users, only: [:index, :show, :create, :update]
       resources :topics, except: [:edit, :new] do
-        resources :posts, only: [:create]
+        resources :posts, except: [:edit, :new]
       end
-      resources :posts, except: [:edit, :new]
     end
   end
 
