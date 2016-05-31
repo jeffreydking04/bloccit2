8220ecff631e1488fdc1982b41f1ef783dd6955f
Jeffrey D King <jdking33@aol.com>
2016-05-31 04:41:20 +0000

    Submit Assignment 46 Revision

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -9,12 +9,10 @@ class Topic < ActiveRecord::Base
   scope :visible_to, -> (user) { user ? all : self.publicly_viewable }
 
   def self.publicly_viewable
-    publicly_viewable_topics = Topic.where(public: true)
-    publicly_viewable_topics
+    Topic.where(public: true)
   end
 
   def self.privately_viewable
-    privately_viewable_topics = Topic.where(public: false)
-    privately_viewable_topics
+    Topic.where(public: false)
   end
 end
